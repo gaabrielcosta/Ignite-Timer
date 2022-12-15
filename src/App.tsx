f630292b82aefd214d1f86d25cba65f194052113
gaabrielcosta <gabriel1414@gmail.com>
2022-12-15 13:48:03 -0200

    Moved context into its own component to share with the history page + Show cycles on history page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { ThemeProvider } from 'styled-components'
 import { Routers } from './Routers'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Routers />
+        <CyclesContextProvider>
+          <Routers />
+        </CyclesContextProvider>
       </BrowserRouter>
 
       <GlobalStyle />
